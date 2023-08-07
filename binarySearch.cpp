4f0dadcfddab704f5462b6b506b0f99bd82add17
Rocky Haque <rockyhaque99@gmail.com>
2023-08-07 10:30:19 +0600

    Create binarySearch.cpp

---
+++ binarySearch.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int binarySearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+
+    int mid = (start + end)/2;
+
+    while(start <= end)
+    {
+        if(arr[mid] == key)
+            return mid;
+
+        // Right Half
+        if(key > arr[mid])
+            start = mid + 1;
+        else
+            end = mid - 1;
+
+        mid = (start + end)/2;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    #ifndef DSA_CPP
+        freopen("input.txt","r", stdin);
+        freopen("output.txt","w", stdout);
+    #endif
+
+        int even[6] = {2, 4, 6, 8, 12, 18};
+        int odd[5] = {3, 8, 11, 14, 16};
+
+        int evenIndex = binarySearch(even, 6, 12);
+        cout<<"Index of 12 is: "<<evenIndex<<endl;
+
+        int oddIndex = binarySearch(odd, 5, 8);
+        cout<<"Index of 8 is: "<<oddIndex<<endl;
+
+    return 0;
+}
